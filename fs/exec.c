87f7930de0420cc3d27501f3f15ab0bffba24348
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2025-11-06 03:00:23 +0000

    Revert "kernelsu: remove the always_inline attribute from fs/exec.c manual hooks"
    This reverts commit 587dcf57fb52b701c10645b75f8ad6e57913d90c.

--- fs/exec.c
+++ fs/exec.c
@@ -1911,7 +1911,7 @@ void set_dumpable(struct mm_struct *mm, int value)
 
 #if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
 extern bool ksu_execveat_hook __read_mostly;
-extern __attribute__((hot)) int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
+extern __attribute__((hot, always_inline)) int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 			       void *__never_use_argv, void *__never_use_envp,
 			       int *__never_use_flags);
 extern int ksu_handle_execve_ksud(const char __user *filename_user,
